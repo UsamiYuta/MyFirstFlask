43b44883f12c955b4f87601bb5fa031a00a54b60
UsamiYuta <Shoegazer.mac@gmail.com>
2020-07-05 10:25:57 +0900

    myNewFlask

---
+++ flask_blog/config.py
@@ -0,0 +1,10 @@
+# flask_blog/config.py
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+DEBUG = True
+SECRET_KEY = 'secret Key'
+USERNAME = 'usami'
+PASSWORD = 'usami'
+
+
